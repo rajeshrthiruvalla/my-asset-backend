1524d2ec2bef2eb5e740d8a4a2a93fde16411c0d
Rajesh <rajesh.r@airpay.technology>
2025-08-13 14:50:14 +0530

    optimization

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,10 @@ const express=require('express')
 const router=express.Router()
 const validate=require('../middleware/validate')
 const {register,login,verifyEmail,forgotPassword,changePassword}=require('../controller/AuthController')
-const registerValidationRules = require('../validations/registerValidation')
+const {registerValidationRules,loginValidationRules,forgotPasswordValidationRules} = require('../validations/authValidation')
 router.post('/register',registerValidationRules, validate,register)
-const loginValidationRules = require('../validations/loginValidation')
 router.post('/login',loginValidationRules, validate,login)
 router.get('/verify-email',verifyEmail)
-const forgotPasswordValidationRules = require('../validations/forgotPasswordValidation')
 router.post('/forgot-password',forgotPasswordValidationRules,validate,forgotPassword)
 
 module.exports=router
--- routes/main.js
+++ routes/main.js
@@ -8,9 +8,8 @@ const {updateProfile,changePassword}=require('../controller/AuthController')
 
 router.use(authentificate);
 router.use(emailVerificate);
-const updateProfileValidationRules=require('../validations/updateProfileValidation')
+const {updateProfileValidationRules,changePasswordValidationRules}=require('../validations/authValidation')
 router.post('/update-profile',updateProfileValidationRules,validate,updateProfile)
-const changePasswordValidationRules = require('../validations/changePasswordValidation')
 router.post('/change-password',changePasswordValidationRules,validate,changePassword)
 
 

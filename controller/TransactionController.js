1ff5c58944887ff8c83299a19744a1ccfc07131f
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-10-01 14:47:39 +0530

    transaction updated

--- controller/TransactionController.js
+++ controller/TransactionController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Transaction=require('../model/Transaction')
 
 const storeTransaction=async (req,res)=>{
@@ -20,7 +21,75 @@ const storeTransaction=async (req,res)=>{
 
 const listTransaction=async (req,res)=>{
     const userId= req.token.userId; 
-    const transactions =await Transaction.find({userId});
+    const transactions = await Transaction.aggregate([
+                          // filter by userId
+                          {
+                            $match: {
+                              userId: new mongoose.Types.ObjectId(userId)
+                            }
+                          },
+
+                          // join fromAccountId -> Account
+                          {
+                            $lookup: {
+                              from: "accounts",
+                              localField: "fromAccountId",
+                              foreignField: "_id",
+                              as: "fromAccount"
+                            }
+                          },
+                          { $unwind: "$fromAccount" },
+
+                          // join fromAccount.iconId -> Icon
+                          {
+                            $lookup: {
+                              from: "icons",
+                              localField: "fromAccount.iconId",
+                              foreignField: "_id",
+                              as: "fromIcon"
+                            }
+                          },
+                          { $unwind: "$fromIcon" },
+
+                          // join toAccountId -> Account
+                          {
+                            $lookup: {
+                              from: "accounts",
+                              localField: "toAccountId",
+                              foreignField: "_id",
+                              as: "toAccount"
+                            }
+                          },
+                          { $unwind: "$toAccount" },
+
+                          // join toAccount.iconId -> Icon
+                          {
+                            $lookup: {
+                              from: "icons",
+                              localField: "toAccount.iconId",
+                              foreignField: "_id",
+                              as: "toIcon"
+                            }
+                          },
+                          { $unwind: "$toIcon" },
+
+                          // select only the fields you need
+                          {
+                            $project: {
+                              fromAccountId:"$fromAccountId",
+                              toAccountId:"$toAccountId",
+                              amount: "$amount",
+                              description: "$description",
+                              entryAt:"$entryAt",
+                              type:"$type",
+                              userId:"$userId",
+                              fromAccount: "$fromAccount.name",
+                              toAccount: "$toAccount.name",
+                              fromIcon: "$fromIcon.path",
+                              toIcon: "$toIcon.path"
+                            }
+                          }
+                        ]);
       res.json({
         message: `Transaction List`,
         data:transactions

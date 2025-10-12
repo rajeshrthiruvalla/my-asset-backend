8c1f7b22327a1a6db28eceaa4db30c40a759c7f3
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-10-12 13:48:18 +0530

    db config updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB=()=>{
-    mongoose.connect('mongodb://localhost:27017/my_asset')
+    mongoose.connect(process.env.DB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 }

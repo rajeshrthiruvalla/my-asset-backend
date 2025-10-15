7ea31d945fbcd4e7c50a21cd1e2dc230be39fbcf
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-10-15 22:10:23 +0530

    google auth checking

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use('/',auth)
 app.use('/',main)
+app.use(express.urlencoded({ extended: true }))
+app.all('/google-auth', (req, res) => {
+  res.render('Params', {
+    method: req.method,
+    queryParams: req.query,
+    bodyParams: req.body
+  });
+})
 app.listen(3000)

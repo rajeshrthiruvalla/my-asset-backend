709ca2dccc634544ff179d5b59c95559fc684a9e
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-10-26 13:30:25 +0530

    delete account link added

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -140,7 +140,7 @@ const login=async (req,res)=>{
             await user.save();
            }
             await sentVerificationMail(user.verificationToken,email);
-            return  res.status(400).json({message:"Verification Link Sent To Your Mobile. Please verify"});
+            return  res.status(400).json({message:"Verification Link Sent To Your Email. Please verify"});
         }
         const token=generateToken(user);
         res.status(201).json({
@@ -213,4 +213,84 @@ const googleLogin=async (req,res)=>{
   res.redirect(intentUrl);
 
 }
-module.exports={register,verifyEmail,login,updateProfile,forgotPassword,changePassword,googleLogin}
+
+const deleteAccount=async (req,res)=>{
+    return res.render('DeleteAccount');
+}
+
+const sentDeactivationMail=async (verificationToken,email)=>{
+    const verificationUrl = `${process.env.FRONTEND_URL}/delete-account-verify-email?token=${verificationToken}&email=${email}`;
+           
+            const mailOptions = {
+                from: `"My Asset" <${process.env.EMAIL_USER}>`,
+                to: email,
+                subject: 'Delete your Account',
+                html: `
+                  <h2>Account Deletion</h2>
+                  <p>Please click the below link to permentently delete your account:</p>
+                  <a href="${verificationUrl}">Verify Email</a>
+                  <p>This link will expire in 1 hour.</p>
+                `
+              };
+
+              await transporter.sendMail(mailOptions);
+}
+
+const deleteAccountSubmit=async(req,res)=>{
+      const { email, password } = req.body;
+      const user=await User.findOne({email});
+        if(!user)
+        {
+          return  res.status(400).json({message:"User Not Registered"});
+        }
+
+        if(!bcrypt.compare(password, user.password))
+        {
+          return  res.status(400).json({message:"Incorrect Password"});
+        }
+
+        if((user.verificationTokenExpiry<Date.now())||(!user.verificationTokenExpiry))
+        {
+          user.isVerified = false;
+          user.verificationToken=crypto.randomBytes(32).toString('hex');
+          user.verificationTokenExpiry=Date.now() + 3600000;
+          await user.save();
+        }
+
+        await sentDeactivationMail(user.verificationToken,email);
+        return  res.status(200).json({message:"Deactivation Link Sent To Your Email. Please verify"});
+
+}
+
+const deleteAccountVerifyEmail = async (req, res) => {
+  try {
+    const { email, token } = req.query;
+    const user = await User.findOne({
+      email,
+      verificationToken: token,
+      verificationTokenExpiry: { $gt: Date.now() }
+    });
+
+    if (!user) {
+       return  res.render('VerificationFailed', {
+            message: "We couldn't verify your email. The link may have expired or is invalid.",
+            secondary:"Retry login, new verification link generate"
+        })
+    }
+
+    user.isVerified = false;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiry = undefined;
+    user.email=`deleted-${email}`;
+    await user.save();
+
+     res.render('DeleteAccountSuccess')
+  } catch (error) {
+    console.error('Email verification error:', error);
+      return  res.render('VerificationFailed', {
+            message: "We couldn't verify your email. Server error during email verification.",
+            secondary:"Retry after some time"
+        })
+  }
+};
+module.exports={register,verifyEmail,login,updateProfile,forgotPassword,changePassword,googleLogin,deleteAccount,deleteAccountSubmit,deleteAccountVerifyEmail}
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 const express=require('express')
 const router=express.Router()
 const validate=require('../middleware/validate')
-const {register,login,verifyEmail,forgotPassword,googleLogin}=require('../controller/AuthController')
+const {register,login,verifyEmail,forgotPassword,googleLogin,deleteAccount,deleteAccountSubmit,deleteAccountVerifyEmail}=require('../controller/AuthController')
 const {registerValidationRules,loginValidationRules,forgotPasswordValidationRules} = require('../validations/authValidation')
 router.post('/register',registerValidationRules, validate,register)
 router.post('/login',loginValidationRules, validate,login)
 router.get('/verify-email',verifyEmail)
 router.post('/forgot-password',forgotPasswordValidationRules,validate,forgotPassword)
-router.get('/google-auth',googleLogin)
+// router.get('/google-auth',googleLogin)
+router.get('/delete-account',deleteAccount);
+router.post('/delete-account-submit',loginValidationRules, validate,deleteAccountSubmit)
+router.get('/delete-account-verify-email',deleteAccountVerifyEmail)
 
 module.exports=router

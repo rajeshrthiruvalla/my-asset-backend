f4d85da385fee4d43e06f4a8fd3e494daea4cb40
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-10-30 23:49:02 +0530

    password validation removed

--- validations/authValidation.js
+++ validations/authValidation.js
@@ -5,11 +5,7 @@ const changePasswordValidationRules = [
   // Password validation
   body('password')
     .notEmpty().withMessage('Password is required')
-    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
-    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
-    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
-    .matches(/[0-9]/).withMessage('Password must contain at least one number')
-    .matches(/[!@#$%^&*]/).withMessage('Password must contain at least one special character (!@#$%^&*)'),
+    .isLength({ min: 4 }).withMessage('Password must be at least 4 characters long'),
 
   // Confirm Password validation
   body('confirm_password')
@@ -68,11 +64,7 @@ const registerValidationRules = [
   // Password validation
   body('password')
     .notEmpty().withMessage('Password is required')
-    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
-    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
-    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
-    .matches(/[0-9]/).withMessage('Password must contain at least one number')
-    .matches(/[!@#$%^&*]/).withMessage('Password must contain at least one special character (!@#$%^&*)'),
+    .isLength({ min: 4 }).withMessage('Password must be at least 4 characters long'),
 
   // Confirm Password validation
   body('confirm_password')

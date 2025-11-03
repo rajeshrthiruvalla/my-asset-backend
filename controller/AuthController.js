bcf82a830b22a84b05043d26eba52f727e3d8313
Rajesh <mailtorajeshthiruvalla@gmail.com>
2025-11-03 20:11:25 +0530

    bug fix

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -211,7 +211,7 @@ const updateProfile=async (req,res)=>{
     if (!updatedUser) {
       throw new Error('User not found');
     }
-    const token=generateToken(user);
+    const token=generateToken(updatedUser);
     return res.status(201).json({"message":"Updated Successfully",
                                  "data":{ name:updatedUser.name, email:updatedUser.email, currency:updatedUser.currency, token, photo:updatedUser.photo }});
   } catch (error) {
